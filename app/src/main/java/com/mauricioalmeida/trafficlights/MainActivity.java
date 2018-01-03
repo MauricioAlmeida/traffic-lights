670d5f4327bf5f6c97ea6dc2c3459f024e473f1d
Mauricio Almeida <mauricio.almeida@uol.com.br>
2018-01-02 23:40:57 -0200

    Initial Implementation of Traffic Lights app

---
+++ app/src/main/java/com/mauricioalmeida/trafficlights/MainActivity.java
@@ -0,0 +1,61 @@
+package com.mauricioalmeida.trafficlights;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private ImageView redLight;
+    private ImageView yellowLight;
+    private ImageView greenLight;
+
+    private Button redButton;
+    private Button yellowButton;
+    private Button greenButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        redLight = (ImageView) findViewById(R.id.red_light);
+        yellowLight = (ImageView) findViewById(R.id.yellow_light);
+        greenLight = (ImageView) findViewById(R.id.green_light);
+
+        redButton = (Button) findViewById(R.id.red_button);
+        yellowButton = (Button) findViewById(R.id.yellow_button);
+        greenButton = (Button) findViewById(R.id.green_button);
+
+        redButton.setOnClickListener(this);
+        yellowButton.setOnClickListener(this);
+        greenButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == redButton) {
+            turnOnRedLight();
+        } else if (v == yellowButton) {
+            turnOnYellowLight();
+        } else if (v == greenButton) {
+            turnOnGreenLight();
+        }
+    }
+
+    private void turnOnRedLight() {
+        redLight.setImageResource(R.drawable.red_on);
+    }
+
+    private void turnOnYellowLight() {
+        yellowLight.setImageResource(R.drawable.yellow_on);
+    }
+
+    private void turnOnGreenLight() {
+        greenLight.setImageResource(R.drawable.green_on);
+    }
+
+}
